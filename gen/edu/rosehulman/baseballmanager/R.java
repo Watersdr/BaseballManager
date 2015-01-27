347fbe851457d908c83f618ca0e005351b804206
Deebow09 <watersdr@rose-hulman.edu>
2015-01-27 10:38:23 -0500

    same as previous

--- gen/edu/rosehulman/baseballmanager/R.java
+++ gen/edu/rosehulman/baseballmanager/R.java
@@ -54,8 +54,6 @@ public final class R {
         public static final int create_team_button=0x7f09002e;
         public static final int defense_stats_list_view=0x7f090011;
         public static final int earned_runs=0x7f09003a;
-        public static final int editText2=0x7f090003;
-        public static final int editText3=0x7f090005;
         public static final int era=0x7f09003d;
         public static final int errors=0x7f090036;
         public static final int first_base_spinner=0x7f09001b;
@@ -71,6 +69,8 @@ public final class R {
         public static final int listview=0x7f090026;
         public static final int mainLayout=0x7f090027;
         public static final int new_first_name=0x7f090001;
+        public static final int new_last_name=0x7f090003;
+        public static final int new_number=0x7f090005;
         public static final int pitcher_name=0x7f090037;
         public static final int pitcher_spinner=0x7f09001a;
         public static final int pitching_stats_list_view=0x7f090028;
