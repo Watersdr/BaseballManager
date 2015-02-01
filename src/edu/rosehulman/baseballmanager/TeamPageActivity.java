f453e119fabf8836630a77225f6db43ce35a3b9e
Deebow09 <watersdr@rose-hulman.edu>
2015-02-01 00:03:22 -0500

    tied together some pages
    still a decent amount to do left

--- src/edu/rosehulman/baseballmanager/TeamPageActivity.java
+++ src/edu/rosehulman/baseballmanager/TeamPageActivity.java
@@ -1,9 +1,13 @@
 package edu.rosehulman.baseballmanager;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class TeamPageActivity extends Activity {
 
@@ -11,6 +15,48 @@ public class TeamPageActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_team_page);
+		
+		Button rosterButton = (Button) findViewById(R.id.roster_button);
+		rosterButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				makeRosterIntent();
+			}
+		});
+		
+		Button scheduleButton = (Button) findViewById(R.id.schedule_button);
+		scheduleButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				makeScheduleIntent();
+			}
+		});
+		Button statsButton = (Button) findViewById(R.id.season_statistics_button);
+		statsButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				makeStatsIntent();
+			}
+		});
+		
+	}
+
+	protected void makeStatsIntent() {
+		Intent i = new Intent(this, SeasonStatisticsActivity.class);
+		startActivity(i);		
+	}
+
+	protected void makeScheduleIntent() {
+		Intent i = new Intent(this, ScheduleActivity.class);
+		startActivity(i);
+	}
+
+	protected void makeRosterIntent() {
+		Intent i = new Intent(this, RosterActivity.class);
+		startActivity(i);
 	}
 
 	@Override
