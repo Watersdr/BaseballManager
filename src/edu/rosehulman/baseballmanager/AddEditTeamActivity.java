6b2eabfca42b074078204fc3ca9e3f177746d0c4
Deebow09 <watersdr@rose-hulman.edu>
2015-02-08 21:45:17 -0500

    started work on creating team

--- src/edu/rosehulman/baseballmanager/AddEditTeamActivity.java
+++ src/edu/rosehulman/baseballmanager/AddEditTeamActivity.java
@@ -1,16 +1,58 @@
 package edu.rosehulman.baseballmanager;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class AddEditTeamActivity extends Activity {
 
+	private ImageView mTeamPicture;
+	private EditText mTeamName;
+	private Bitmap mTeamBitmap;
+	
+	private static final int SELECT_PHOTO = 100;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_create_team);
+		
+		
+		Button saveButton = (Button) findViewById(R.id.save_button);
+		mTeamPicture = (ImageView) findViewById(R.id.team_picture);
+		mTeamName = (EditText) findViewById(R.id.new_team_name);
+		
+		mTeamPicture.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent photoSelectorIntent = new Intent(Intent.ACTION_PICK);
+				photoSelectorIntent.setType("image/*");
+				startActivityForResult(photoSelectorIntent, SELECT_PHOTO);
+			}
+		});
+		
+		saveButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				String name = mTeamName.getText().toString();
+				Team newTeam = new Team(name, mTeamBitmap);
+			}
+		});
 	}
 
 	@Override
@@ -30,5 +72,28 @@ public class AddEditTeamActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		
+		switch (requestCode) {
+		case SELECT_PHOTO:
+			if (resultCode == RESULT_OK) {
+				Uri image = data.getData();
+				InputStream imageStream;
+				try {
+					imageStream = getContentResolver().openInputStream(image);
+					Bitmap selected = BitmapFactory.decodeStream(imageStream);
+					mTeamPicture.setImageBitmap(selected);
+					mTeamBitmap = selected;
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+				
+		
 	}
 }
